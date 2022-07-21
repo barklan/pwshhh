9669cb759dd74c7e79e51b9dd0460736aa09cd3c
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-07-21 11:00:15 +0300

    feat: csv files

--- simple.ps1
+++ simple.ps1
@@ -10,3 +10,13 @@ Get-Process f* | Select-Object -Property name, @{name='proooooo';expression={$_.
 
 Get-Process | Where-Object {$_.Handles -gt 100}
 Get-Process | where handles -gt 100
+
+Get-Process | where handles -gt 100 | Sort-Object -Property Handles | ft Name, Handles -AutoSize
+
+$procs = Get-Process | where Handles -gt 100
+
+Write-Output "john" | wl-copy
+
+Get-Content ./README.md
+
+Get-Process | Export-Csv procs.csv
