4ca98212c1abf142de26aadf8e928bf5fa5473d9
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-07-21 14:03:14 +0300

    feat: objects and comparing

--- objects.ps1
+++ objects.ps1
@@ -1,3 +1,7 @@
 Get-Process | Measure-Object WS -Sum -Maximum -Minimum -Average
 
 Get-Process | Sort-Object ws -Descending | select -First 5
+
+$procs = Get-Process
+$procs2 = Get-Process
+Compare-Object -ReferenceObject $procs -DifferenceObject $procs2 -Property Name
